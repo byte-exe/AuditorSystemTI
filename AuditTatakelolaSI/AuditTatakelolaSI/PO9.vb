97717493e42f02f5338a020d0b73abeb65394d05
Vyneee <rivaldo_erdany@yahoo.com>
2022-03-30 02:47:25 +0700

    update

--- AuditTatakelolaSI/AuditTatakelolaSI/PO9.vb
+++ AuditTatakelolaSI/AuditTatakelolaSI/PO9.vb
@@ -1,9 +1,17 @@
 ﻿Public Class PO9
 
     Dim a1, a2, a3, a4, a5, a6, a7, a8 As Integer
+    Dim b1, b2, b3, b4, b5, b6, b7, b8 As Integer
+    Dim c1, c2, c3, c4, c5, c6, c7, c8 As Integer
+    Dim d1, d2, d3, d4, d5, d6, d7, d8 As Integer
+    Dim e1, e2, e3, e4, e5, e6, e7, e8 As Integer
    
-
     Dim total_a As Integer
+    Dim total_b As Integer
+    Dim total_c As Integer
+    Dim total_d As Integer
+    Dim total_e As Integer
+
 
 
     Private Sub Label15_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label15.Click
@@ -109,6 +117,84 @@
     End Sub
 
     Private Sub P_1_4_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles P_1_4.CheckedChanged
+        If P_1_4.Checked Then
+            b1 = 4
+        Else
+            b1 = 0
+        End If
+    End Sub
+
+    Private Sub TextBox2_MouseMove(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles TextBox2.MouseMove
+        total_b = b1 + b2 + b3 + b4 + b5 + b6 + b7 + b8
+        TextBox2.Text = total_b
+    End Sub
+
+
+    Private Sub TextBox2_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TextBox2.TextChanged
+
+    End Sub
+
+    Private Sub P_2_4_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles P_2_4.CheckedChanged
+        If P_2_4.Checked Then
+            b2 = 4
+        Else
+            b2 = 0
+        End If
+    End Sub
+
+    Private Sub P_3_4_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles P_3_4.CheckedChanged
+        If P_3_4.Checked Then
+            b3 = 4
+        Else
+            b3 = 0
+        End If
+    End Sub
+
+    Private Sub P_4_4_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles P_4_4.CheckedChanged
+        If P_4_4.Checked Then
+            b4 = 4
+        Else
+            b4 = 0
+        End If
+    End Sub
+
+    Private Sub P_5_4_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles P_5_4.CheckedChanged
+        If P_5_4.Checked Then
+            b5 = 4
+        Else
+            b5 = 0
+        End If
+    End Sub
+
+    Private Sub P_6_4_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles P_6_4.CheckedChanged
+        If P_6_4.Checked Then
+            b6 = 4
+        Else
+            b6 = 0
+        End If
+    End Sub
+
+    Private Sub P_7_4_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles P_7_4.CheckedChanged
+        If P_7_4.Checked Then
+            b7 = 4
+        Else
+            b7 = 0
+        End If
+    End Sub
+
+    Private Sub P_8_4_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles P_8_4.CheckedChanged
+        If P_8_4.Checked Then
+            b8 = 4
+        Else
+            b8 = 0
+        End If
+    End Sub
+
+    Private Sub Panel1_Paint(ByVal sender As System.Object, ByVal e As System.Windows.Forms.PaintEventArgs) Handles Panel1.Paint
+
+    End Sub
+
+    Private Sub Panel6_Paint(ByVal sender As System.Object, ByVal e As System.Windows.Forms.PaintEventArgs) Handles Panel6.Paint
 
     End Sub
 End Class
